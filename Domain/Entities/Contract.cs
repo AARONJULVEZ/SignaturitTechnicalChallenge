7de549e7e34f56e4e817c288ffc44f8dc8dc2e1c
AARONJULVEZ <aa.julvez@gmail.com>
2023-06-06 14:47:35 +0200

    Añadimos la entidad principal del ejercicio, esta aceptará dos cadenas de texto como entrada, representando al demandante y al demandado, trabajará tambien la posibilidad de admitir un caracter # con la finalidad de usar una misma entidad para ambas fases.

---
+++ Domain/Entities/Contract.cs
@@ -0,0 +1,111 @@
+﻿using System.Text.RegularExpressions;
+using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Contract
+    {
+        private Contract(string signatureCollection, int signatureValue)
+        {
+            Signatures = signatureCollection;
+            SignaturesValue = signatureValue;
+        }
+
+        public string Signatures { get; }
+        public int SignaturesValue { get; }
+
+        public static Result<Contract> Create(string signatures)
+        {
+            if (string.IsNullOrWhiteSpace(signatures))
+            {
+                Error error = new Error(400, "Signatures field cannot be empty");
+                return Result<Contract>.Fail(null, error);
+            }
+
+            var missingSignaturesCount = Regex.Matches(signatures, "[#]").Count;
+            if (missingSignaturesCount > 1)
+            {
+                Error error = new Error(400, "It can be up to just one missing signature");
+                return Result<Contract>.Fail(null, error);
+            }
+
+            if (!CheckStringPattern(missingSignaturesCount == 1 ? signatures.Replace("#", "") : signatures))
+            {
+                Error error = new Error(400, "Signatures field must contain a combination of just the letters K, N or V");
+                return Result<Contract>.Fail(null, error);
+            }
+
+            var signaturesValue = ResolveSignatureCollection(missingSignaturesCount == 1 ? signatures.Replace("#", "") : signatures);
+
+            return Result<Contract>.Correct(new Contract(signatures, signaturesValue));
+        }
+
+        public ComparisonStatus CompareWithRival(Contract rival)
+        {
+            if (SignaturesValue > rival.SignaturesValue)
+                return ComparisonStatus.Win;
+
+            if (SignaturesValue == rival.SignaturesValue)
+                return ComparisonStatus.Draw;
+
+            return ComparisonStatus.Lose;
+        }
+
+        public RolesSignatureValue? GetMinimunSignatureNeededToWin(Contract rival)
+        {
+            var spread = rival.SignaturesValue - SignaturesValue;
+            if (spread >= 0 && spread <= 4)
+            {
+                if (spread == 0)
+                {
+                    return RolesSignatureValue.Validator;
+                }
+                else if (spread == 1)
+                {
+                    return RolesSignatureValue.Notary;
+                }
+                else
+                {
+                    var signaturesValue = ResolveSignatureCollection(Signatures.Replace("#", "K"));
+                    if (rival.SignaturesValue - signaturesValue < 0)
+                    {
+                        return RolesSignatureValue.King;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static bool CheckStringPattern(string signatures)
+        {
+            if (!Regex.IsMatch(signatures, "^[KNVknv]+$"))
+                return false;
+
+            return true;
+        }
+
+        private static int ResolveSignatureCollection(string signatures)
+        {
+            var amountOfKingsSignatures = Regex.Matches(signatures, "[Kk]").Count;
+            var valueOfKingSignature = 5;
+            var kingSignatureResult = amountOfKingsSignatures * valueOfKingSignature;
+
+            var amountOfNotarySignatures = Regex.Matches(signatures, "[Nn]").Count;
+            var valueOfNotarySignature = 2;
+            var notarySignatureResult = amountOfNotarySignatures * valueOfNotarySignature;
+
+            var amountOfValidators = Regex.Matches(signatures, "[Vv]").Count;
+            var valueOfValidatorSignature = amountOfKingsSignatures == 0 ? 1 : 0;
+            var validatorSignatureResult = amountOfValidators * valueOfValidatorSignature;
+
+            return kingSignatureResult + notarySignatureResult + validatorSignatureResult;
+        }
+    }
+}
---
+++ Testing/Domain.Testing/Entities/ContratTest.cs
@@ -0,0 +1,112 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using NUnit.Framework;
+
+namespace Domain.Test.Entities
+{
+    [TestFixture]
+    public class ContractTest
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Create_Contract_returns_error_if_signature_input_is_empty_or_white_space(string signatures)
+        {
+            var contract = Contract.Create(signatures);
+
+            Assert.True(contract.IsFail);
+            Assert.False(contract.IsCorrect);
+            Assert.NotNull(contract.Error);
+            Assert.IsTrue(contract.Error.Code == 400);
+            Assert.IsTrue(contract.Error.Message == "Signatures field cannot be empty");
+        }
+
+        [Test]
+        public void Create_Contract_returns_error_if_signature_input_has_more_than_one_hashtag()
+        {
+            var signatures = "K#VV#";
+            var contract = Contract.Create(signatures);
+
+            Assert.True(contract.IsFail);
+            Assert.False(contract.IsCorrect);
+            Assert.NotNull(contract.Error);
+            Assert.IsTrue(contract.Error.Code == 400);
+            Assert.IsTrue(contract.Error.Message == "It can be up to just one missing signature");
+        }
+
+        [TestCase("KMV")]
+        [TestCase("NNB")]
+        [TestCase("K1#")]
+        public void Create_Contract_returns_error_if_signature_input_has_invalid_characters(string signatures)
+        {
+            var contract = Contract.Create(signatures);
+
+            Assert.True(contract.IsFail);
+            Assert.False(contract.IsCorrect);
+            Assert.NotNull(contract.Error);
+            Assert.IsTrue(contract.Error.Code == 400);
+            Assert.IsTrue(contract.Error.Message == "Signatures field must contain a combination of just the letters K, N or V");
+        }
+
+        [TestCase("KNV", 7)]
+        [TestCase("NNV", 5)]
+        [TestCase("VVV", 3)]
+        public void Create_Contract_calculate_signatures_value(string signatures, int value)
+        {
+            var contract = Contract.Create(signatures);
+
+            Assert.True(contract.IsCorrect);
+            Assert.False(contract.IsFail);
+            Assert.Null(contract.Error);
+            Assert.IsTrue(contract.Value.SignaturesValue == value);
+        }
+
+        [TestCase("KnNnV", ComparisonStatus.Win)]
+        [TestCase("NnN", ComparisonStatus.Draw)]
+        [TestCase("Vv", ComparisonStatus.Lose)]
+        public void CompareWithRival_returns_the_winner_in_a_trial(string signatures, ComparisonStatus status)
+        {
+            var contract = Contract.Create(signatures);
+            var rivalSignatures = "NNN";
+            var rival = Contract.Create(rivalSignatures);
+
+            Assert.True(contract.IsCorrect);
+            Assert.True(rival.IsCorrect);
+            var result = contract.Value.CompareWithRival(rival.Value);
+
+            Assert.True(result == status);
+        }
+
+        [TestCase("K#NV", RolesSignatureValue.Notary)]
+        [TestCase("NN#NN", RolesSignatureValue.Validator)]
+        [TestCase("NN#V", RolesSignatureValue.King)]
+        public void GetMinimunSignatureNeededToWin_returns_the_necesary_role_for_win(string signatures, RolesSignatureValue role)
+        {
+            var contract = Contract.Create(signatures);
+            var rivalSignatures = "NNNN";
+            var rival = Contract.Create(rivalSignatures);
+
+            Assert.True(contract.IsCorrect);
+            Assert.True(rival.IsCorrect);
+            var result = contract.Value.GetMinimunSignatureNeededToWin(rival.Value);
+
+            Assert.NotNull(result);
+            Assert.True(result.Value == role);
+        }
+
+        [Test]
+        public void GetMinimunSignatureNeededToWin_returns_null_if_needs_more_than_one_signature_to_win()
+        {
+            var signatures = "K#VV";
+            var contract = Contract.Create(signatures);
+            var rivalSignatures = "KNNNN";
+            var rival = Contract.Create(rivalSignatures);
+
+            Assert.True(contract.IsCorrect);
+            Assert.True(rival.IsCorrect);
+            var result = contract.Value.GetMinimunSignatureNeededToWin(rival.Value);
+
+            Assert.IsNull(result);
+            Assert.True((rival.Value.SignaturesValue - contract.Value.SignaturesValue) > 4);
+        }
+    }
+}
