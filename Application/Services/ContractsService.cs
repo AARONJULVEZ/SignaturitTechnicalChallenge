88646a312a343c61376d2645ed73f34af452df87
AARONJULVEZ <aa.julvez@gmail.com>
2023-06-06 15:37:25 +0200

    Creamos el servicio y la lógica necesaria para resolver el primer caso de uso, tambien las pruebas que definen y aseguran el correcto curso del desarrollo. Para este primer ejercicio tendremos en cuenta que posteriormente la entidad deberá poder trabajar con el caracter #

---
+++ Application/Services/ContractsService.cs
@@ -0,0 +1,27 @@
+﻿using Application.Queries;
+using Application.Returns;
+using Domain.Common;
+using Domain.Entities;
+
+namespace Application.Services
+{
+    public class ContractsService
+    {
+        public Result<SignatureVerifyResponse> SignatureVerify(SignatureVerifyRequest request)
+        {
+            var plaintiffContract = Contract.Create(request.Plaintiff);
+            if (plaintiffContract.IsFail)
+            {
+                return Result<SignatureVerifyResponse>.Fail(null, plaintiffContract.Error);
+            }
+
+            var defendantContract = Contract.Create(request.Defendant);
+            if (defendantContract.IsFail)
+            {
+                return Result<SignatureVerifyResponse>.Fail(null, defendantContract.Error);
+            }
+
+            return new SignatureVerifyResponse(plaintiffContract.Value, defendantContract.Value);
+        }
+    }
+}
---
+++ Testing/Application.Testing/Returns/SignatureVerifyResponseTest.cs
@@ -0,0 +1,33 @@
+﻿using Application.Returns;
+using Domain.Entities;
+using NUnit.Framework;
+
+namespace Application.Testing.Returns
+{
+    [TestFixture]
+    public class SignatureVerifyResponseTest
+    {
+        [TestCase("K", "VV", "Plaintiff wins to the defendant")]
+        [TestCase("VV", "N", "Plaintiff and defendant are in a draw")]
+        [TestCase("V", "N", "Plaintiff loses to the defendant")]
+        public void SignatureVerifyResponse_Create_returns_new_SignatureVerifyResponse(string plaintiff, string defendant, string recap)
+        {
+            var plaintiffContract = Contract.Create(plaintiff);
+
+            Assert.True(plaintiffContract.IsCorrect);
+            Assert.False(plaintiffContract.IsFail);
+            Assert.IsNull(plaintiffContract.Error);
+
+            var defendantContract = Contract.Create(defendant);
+
+            Assert.True(defendantContract.IsCorrect);
+            Assert.False(defendantContract.IsFail);
+            Assert.IsNull(defendantContract.Error);
+
+            var response = new SignatureVerifyResponse(plaintiffContract.Value, defendantContract.Value);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(recap, response.Recap);
+        }
+    }
+}
