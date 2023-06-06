79254ab96ae0d4ad20b265d7e362158100d94b01
AARONJULVEZ <aa.julvez@gmail.com>
2023-06-06 14:38:35 +0200

    Establecemos un objeto genérico de respuesta junto con uno de error que usaremos a lo largo de toda la aplicación, creamos un conjunto de pruebas unitarias para garantizar la calidad y asegurar el funcionamiento de determinadas casuísticas.

---
+++ Testing/Domain.Testing/Common/ResultTest.cs
@@ -0,0 +1,96 @@
+﻿using Domain.Common;
+using NUnit.Framework;
+
+namespace Domain.Test.Common
+{
+    [TestFixture]
+    public class ResultTest
+    {
+        [Test]
+        public void Result_by_default_is_Correct()
+        {
+            var result = Result.Correct();
+
+            Assert.True(result.IsCorrect);
+            Assert.False(result.IsFail);
+            Assert.Null(result.Error);
+        }
+
+        [Test]
+        public void Result_throws_exception_at_creating_Fail_result_without_Error()
+        {
+            var result = Result.Fail(null);
+
+            Assert.True(result.IsFail);
+            Assert.False(result.IsCorrect);
+            Assert.NotNull(result.Error);
+            Assert.IsTrue(result.Error.Code == 500);
+            Assert.IsTrue(result.Error.Message == "Response mapping error, null error value");
+        }
+
+        [Test]
+        public void Result_without_Error_is_Fail()
+        {
+            var error = new Error(1, "ONE");
+            var result = Result.Fail(error);
+
+            Assert.True(result.IsFail);
+            Assert.False(result.IsCorrect);
+            Assert.NotNull(result.Error);
+            Assert.IsTrue(result.Error.Code == 1);
+            Assert.IsTrue(result.Error.Message == "ONE");
+        }
+
+        [Test]
+        public void Correct_result_returns_value()
+        {
+            var value = new object();
+            var result = Result<object>.Correct(value);
+
+            Assert.True(result.IsCorrect);
+        }
+
+        [Test]
+        public void Correct_result_with_null_throws_exception()
+        {
+            var result = Result<object>.Correct(null);
+
+            Assert.True(result.IsFail);
+            Assert.False(result.IsCorrect);
+            Assert.NotNull(result.Error);
+            Assert.IsTrue(result.Error.Code == 500);
+            Assert.IsTrue(result.Error.Message == "Response mapping error, null value");
+        }
+
+        [Test]
+        public void Fail_result_without_Error_throws_exception()
+        {
+            var result = Result<object>.Fail(new object(), null);
+
+            Assert.True(result.IsFail);
+            Assert.False(result.IsCorrect);
+            Assert.NotNull(result.Error);
+            Assert.IsTrue(result.Error.Code == 500);
+            Assert.IsTrue(result.Error.Message == "Response mapping error, null error value");
+        }
+
+        [Test]
+        public void Fail_result_with_null_and_Error_is_fail()
+        {
+            var error = new Error(1, "ONE");
+            var result = Result<object>.Fail(null, error);
+
+            Assert.True(result.IsFail);
+            Assert.Null(result.Value);
+        }
+
+        [Test]
+        public void Correct_Result_when_assign_value()
+        {
+            var value = new List<object>();
+            Result<IEnumerable<object>> result = value;
+
+            Assert.True(result.IsCorrect);
+        }
+    }
+}
