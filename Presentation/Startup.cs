583358772ccca7922fc1234321c561459f1b2ba9
AARONJULVEZ <aa.julvez@gmail.com>
2023-06-06 14:29:47 +0200

    Creamos la estructura inicial del proyecto junto con sus proyectos de test y le añadiremos una clase StartUp por comodidad, para el proyecto usaremos .net 7, construiremos una arquitectura hexagonal basándonos en DDD, intentaremos implementar lo máximo posible CQRS, TDD, Clean Code, principios SOLID y respecto al Technical Challenge realizaremos las siguientes asunciones:
    Los contratos deben poseer al menos una firma de alguno de los tres roles.
    No hay límite de firmas en un contrato para ninguno de los roles.
    La entrada puede darse tanto en mayúsculas como en minúsculas.
    Se puede ganar, perder o empatar (en caso de no existir la tercera opción desconozco el factor de desempate).
    Las tareas se resuelven en endpoints distintos.

---
+++ Presentation/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Presentation
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.OAuthClientId("");
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            AddSwagger(services);
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = "Lobby Wars API",
+                    Description = "Signaturit Technical Challenge. Aarón Júlvez Manero"
+                });
+            });
+        }
+    }
+}
