114ad2381ea6159af775f8a4ee2db4d43495f094
AARONJULVEZ <aa.julvez@gmail.com>
2023-06-06 16:21:57 +0200

    Finalmente añadimos el controlador a traves del cual podremos acceder mediante http a los dos endpoints correspondientes, agregamos el scope del servicio al startup, junto con las pruebas de integración que verifican el comportamiento esperado de los dos casos de uso.

--- Testing/Application.Testing/Services/ContractsServiceTest.cs
+++ Testing/Application.Testing/Services/ContractsServiceTest.cs
@@ -67,5 +67,62 @@ namespace Application.Testing.Services
             Assert.IsTrue(result.Error.Code == code);
             Assert.IsTrue(result.Error.Message.Equals(error));
         }
+
+        [TestCase("N#", "VV", "Plaintiff needs the signature of a Validator to win", 'V')]
+        [TestCase("KV#", "NNVV", "Plaintiff needs the signature of a Notary to win", 'N')]
+        [TestCase("N#", "VVVV", "Plaintiff needs the signature of a King to win", 'K')]
+        [TestCase("VV", "N#", "Defendant needs the signature of a Validator to win", 'V')]
+        [TestCase("NNVV", "KV#", "Defendant needs the signature of a Notary to win", 'N')]
+        [TestCase("VVVV", "N#", "Defendant needs the signature of a King to win", 'K')]
+        public void FillMissingSignature_returns_new_FillMissingSignatureResponse(string plaintiff, string defendant, string recap, char missingSignature)
+        {
+            var request = FillMissingSignatureRequest.Create(plaintiff, defendant);
+
+            Assert.True(request.IsCorrect);
+
+            var result = _service.FillMissingSignature(request.Value);
+
+            Assert.True(result.IsCorrect);
+            Assert.False(result.IsFail);
+            Assert.IsNull(result.Error);
+
+            Assert.True(result.Value.Recap.Equals(recap));
+            Assert.True(result.Value.MissingSignature.Equals(missingSignature));
+        }
+
+        [TestCase("K1#", "VV", 400, "Signatures field must contain a combination of just the letters K, N or V")]
+        [TestCase("VV#", "K1", 400, "Signatures field must contain a combination of just the letters K, N or V")]
+        public void FillMissingSignature_returns_error_if_Plaintiff_or_Defendant_contract_creation_fails_because_of_invalid_characters(string plaintiff, string defendant, int code, string error)
+        {
+            var request = FillMissingSignatureRequest.Create(plaintiff, defendant);
+
+            Assert.True(request.IsCorrect);
+
+            var result = _service.FillMissingSignature(request.Value);
+
+            Assert.True(result.IsFail);
+            Assert.False(result.IsCorrect);
+            Assert.NotNull(result.Error);
+            Assert.IsTrue(result.Error.Code == code);
+            Assert.IsTrue(result.Error.Message.Equals(error));
+        }
+
+        [TestCase(" ", "#VV", 400, "Signatures field cannot be empty")]
+        [TestCase("V#V", " ", 400, "Signatures field cannot be empty")]
+        public void FillMissingSignature_returns_error_if_Plaintiff_or_Defendant_contract_creation_fails_because_of_empty_input(string plaintiff, string defendant, int code, string error)
+        {
+            var request = FillMissingSignatureRequest.Create(plaintiff, defendant);
+
+            Assert.True(request.IsCorrect);
+            Assert.False(request.IsFail);
+
+            var result = _service.FillMissingSignature(request.Value);
+
+            Assert.True(result.IsFail);
+            Assert.False(result.IsCorrect);
+            Assert.NotNull(result.Error);
+            Assert.IsTrue(result.Error.Code == code);
+            Assert.IsTrue(result.Error.Message.Equals(error));
+        }
     }
 }
---
+++ Testing/Presentation.Testing/Controllers/SignatureControllerTest.cs
@@ -0,0 +1,108 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Presentation.Testing.Models;
+
+namespace Presentation.Testing.Controllers
+{
+    [TestFixture]
+    public class SignatureControllerTest
+    {
+        private WebApplicationFactory<Startup> _factory;
+
+        [SetUp]
+        public void ConfigurarPrueba()
+        {
+            _factory = new WebApplicationFactory<Startup>();
+        }
+
+        [TearDown]
+        public void FinalizarPrueba()
+        {
+            _factory.Dispose();
+        }
+
+        [TestCase("K", "VV", "Plaintiff wins to the defendant")]
+        [TestCase("VV", "N", "Plaintiff and defendant are in a draw")]
+        [TestCase("V", "N", "Plaintiff loses to the defendant")]
+        public async Task SignatureVerify_returns_winner(string plaintiff, string defendant, string recap)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync($"/Signature/{plaintiff}/{defendant}/SignatureVerify");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<SignatureVerifyResponseTestDTO>(stringResponse);
+
+            Assert.NotNull(result);
+            Assert.AreEqual(result.Recap, recap);
+            Assert.AreEqual(result.Plaintiff.Signatures, plaintiff);
+            Assert.AreEqual(result.Defendant.Signatures, defendant);
+        }
+
+        [TestCase("K1", "VV", 400, "Signatures field must contain a combination of just the letters K, N or V")]
+        [TestCase("VV", "K1", 400, "Signatures field must contain a combination of just the letters K, N or V")]
+        public async Task SignatureVerify_returns_error_because_of_invalid_characters(string plaintiff, string defendant, int code, string error)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync($"/Signature/{plaintiff}/{defendant}/SignatureVerify");
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(stringResponse, error);
+        }
+
+        /*
+        Test fails because when you send an empty string, it makes the url not legible and the outcome 404 Not Found
+
+        [TestCase(" ", "VV", 400, "Signatures field cannot be empty")]
+        [TestCase("VV", "", 400, "Signatures field cannot be empty")]
+        public async Task SignatureVerify_returns_error_because_of_empty_input(string plaintiff, string defendant, int code, string error)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync($"/Signature/{plaintiff}/{defendant}/SignatureVerify");
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(stringResponse, error);
+        }
+
+        Test fails because when you send the character #, it makes the url not legible and the outcome 404 Not Found
+        Besides that it works fine, you can see it if you remove the #
+
+        [TestCase("VV#", "VV", "Plaintiff needs the signature of a Validator to win", 'V')]
+        [TestCase("VV#", "NV", "Plaintiff needs the signature of a Notary to win", 'N')]
+        [TestCase("V#", "NV", "Plaintiff needs the signature of a King to win", 'K')]
+        [TestCase("N", "V#V", "Defendant needs the signature of a Validator to win", 'V')]
+        [TestCase("VN", "VV#", "Defendant needs the signature of a Notary to win", 'N')]
+        [TestCase("VN", "#V", "Defendant needs the signature of a King to win", 'K')]
+        public async Task FillMissingSignatureResponse_returns_missing_signature(string plaintiff, string defendant, string recap, char missingSignature)
+        {
+            var client = _factory.CreateClient();
+            var url = $"/Signature/{plaintiff}/{defendant}/FillMissingSignature";
+
+            var response = await client.GetAsync(url);
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<FillMissingSignatureResponseTestDto>(stringResponse);
+
+            Assert.NotNull(result);
+            Assert.AreEqual(result.Plaintiff.Signatures, plaintiff);
+            Assert.AreEqual(result.Defendant.Signatures, defendant);
+            Assert.AreEqual(result.Recap, recap);
+            Assert.AreEqual(result.MissingSignature, missingSignature);
+        }
+        */
+    }
+}
